95e8a822bfc144086c0e8e4cb51f509608ea1ba3
andrinklarer <andrin.klarer@noseryoung.ch>
2021-03-03 15:19:15 +0100

    Inputs/Outputs gespeichert

--- 20210303-Snackomat/src/IO.java
+++ 20210303-Snackomat/src/IO.java
@@ -1,3 +1,13 @@
+import java.util.Scanner;
+
 public class IO {
-    
+    public void inputOutput(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Product Number: ");
+        int productNumber = scanner.nextInt();
+        System.out.println("GIVE ME YOUR MONEY!!!");
+        double money = scanner.nextDouble();
+        System.out.println("Press 1 to Complete (Anything else to stop the Process)");
+        int breakVar = scanner.nextInt();
+    }
 }
