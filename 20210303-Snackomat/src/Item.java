e26ac7309f15ed914f562cd91982a9dd7c759bec
ProgrammStude <shawn.lacarta@yahoo.com>
2021-03-04 10:02:42 +0100

    aufbau

--- 20210303-Snackomat/src/Item.java
+++ 20210303-Snackomat/src/Item.java
@@ -1,2 +1,14 @@
 public class Item {
+
+    private double price;
+    private int amount;
+    private int productId;
+    private String name;
+
+    public Item(double price, int amount, int productId, String name) {
+        this.price = price;
+        this.amount = amount;
+        this.productId = productId;
+        this.name = name;
+    }
 }
--- 20210303-Snackomat/src/VendingMachine.java
+++ 20210303-Snackomat/src/VendingMachine.java
@@ -1,2 +1,45 @@
+import java.util.ArrayList;
+
 public class VendingMachine {
+
+    private ArrayList items;
+    private double register;
+    private int[] size;
+    private int key;
+
+    public VendingMachine(ArrayList items, double register, int[] size, int key) {
+        this.items = items;
+        this.register = register;
+        this.size = size;
+        this.key = key;
+    }
+
+    public void buy(){
+
+    }
+
+    public void fill(){
+
+    }
+
+    public int changePrice(int){
+
+    }
+
+    public int checkAndReturnMoney(int){
+
+    }
+
+    public void changeItem(){
+
+    }
+
+    public void printAutomat(){
+
+    }
+
+    public boolean checkKey(){
+        
+    }
+
 }
