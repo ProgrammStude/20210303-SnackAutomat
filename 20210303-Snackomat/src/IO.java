ded9cdbe165454b397d7c4dbe07308e83027cf2a
ProgrammStude <shawn.lacarta@yahoo.com>
2021-03-05 15:16:55 +0100

    java: add case 4

--- 20210303-Snackomat/src/IO.java
+++ 20210303-Snackomat/src/IO.java
@@ -52,7 +52,7 @@ public class IO {
         }
     }
 
-    public boolean loginAdmin(VendingMachine vendingMachine) {
+    public boolean loginAdmin(VendingMachine vendingMachine, double register) {
         int productInt;
         String productNumber = "";
         String productName;
@@ -64,7 +64,7 @@ public class IO {
         }
         System.out.println("]\n");
         System.out.println("What do you want to do?\nRefill Machine(1), Change Prize of a Product(2)," +
-                " Swap a Product(3)");
+                " Swap a Product(3), Show how much money you have(4)");
         String action = Methods.readSpecInput(vendingMachine.getKey(), stopVar, 1, 3);
         if (abortProcess(action)) return false;
         int actionInt = Methods.parseStringToInt(action);
@@ -103,6 +103,8 @@ public class IO {
                     if (productName == "x")return false;
                 } while (!(vendingMachine.changeItem(productInt, productName)));
                 break;
+            case 4:
+                System.out.println(register);
             default:
                 break;
         }
